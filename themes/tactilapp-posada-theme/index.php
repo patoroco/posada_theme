3a9777f0aef02e4f2851fdd4d75020ef22de98b6
Jorge Maroto <patoroco@gmail.com>
2013-03-09 21:20:09 +0100

    Añadiendo Bootstrap

--- themes/tactilapp-posada-theme/index.php
+++ themes/tactilapp-posada-theme/index.php
@@ -4,36 +4,43 @@
 	<meta charset=utf-8>
 	<title>Posada Fuentes Carrionas</title>
 	
+	<link href="<?php echo get_stylesheet_directory_uri(); ?>/css/bootstrap.min.css" rel="stylesheet" media="screen">
 	<link rel="stylesheet/less" href="<?php echo get_stylesheet_directory_uri(); ?>/css/style.less" />
-		<script type="text/javascript">
-		    less = {
-		        env: "development", // or "production"
-		        async: true,       // load imports async
-		        fileAsync: false,   // load imports async when in a page under
-		                            // a file protocol
-		        poll: 1000,         // when in watch mode, time in ms between polls
-		        functions: {},      // user functions, keyed by name
-		        dumpLineNumbers: "comments", // or "mediaquery" or "all"
-		        relativeUrls: false,// whether to adjust url's to be relative
-		                            // if false, url's are already relative to the
-		                            // entry less file
-		        // rootpath: ":/a.com/"// a path to add on to the start of every url
-		                            //resource
-		    };
-		</script>
+
+	<script type="text/javascript">
+		// Esto se eliminará en la versión final, ya que el CSS irá compilado con LESSCompiler
+	
+	    less = {
+	        env: "development", // or "production"
+	        async: true,       // load imports async
+	        fileAsync: false,   // load imports async when in a page under
+	                            // a file protocol
+	        poll: 1000,         // when in watch mode, time in ms between polls
+	        functions: {},      // user functions, keyed by name
+	        dumpLineNumbers: "comments", // or "mediaquery" or "all"
+	        relativeUrls: false,// whether to adjust url's to be relative
+	                            // if false, url's are already relative to the
+	                            // entry less file
+	        // rootpath: ":/a.com/"// a path to add on to the start of every url
+	                            //resource
+	    };
+	</script>
 	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/less.js" type="text/javascript"></script>
 </head>
-<body>
 
-	<header id="header">
-		<div id="headerbg"></div>
-		<div class="container">
-		</div>
-	</header>
+<body>
+<header id="header">
+	<div id="headerbg"></div>
+	<div class="container">
+	</div>
+</header>
 
 <section id="home" data-type="page" data-name="home">
-		<div id="background"></div>
+	<div id="background"></div>
 </section>
 
+
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap.min.js"></script>
 </body>
 </html>
