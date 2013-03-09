349fd3a4ea832246676036848ae2667576e6b2c0
Jorge Maroto <patoroco@gmail.com>
2013-03-09 21:08:11 +0100

    Añadiendo LESS

---
+++ themes/tactilapp-posada-theme/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset=utf-8>
+	<title>Posada Fuentes Carrionas</title>
+	
+	<link rel="stylesheet/less" href="<?php echo get_stylesheet_directory_uri(); ?>/css/style.less" />
+		<script type="text/javascript">
+		    less = {
+		        env: "development", // or "production"
+		        async: true,       // load imports async
+		        fileAsync: false,   // load imports async when in a page under
+		                            // a file protocol
+		        poll: 1000,         // when in watch mode, time in ms between polls
+		        functions: {},      // user functions, keyed by name
+		        dumpLineNumbers: "comments", // or "mediaquery" or "all"
+		        relativeUrls: false,// whether to adjust url's to be relative
+		                            // if false, url's are already relative to the
+		                            // entry less file
+		        // rootpath: ":/a.com/"// a path to add on to the start of every url
+		                            //resource
+		    };
+		</script>
+	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/less.js" type="text/javascript"></script>
+</head>
+<body>
+
+	<header id="header">
+		<div id="headerbg"></div>
+		<div class="container">
+		</div>
+	</header>
+
+<section id="home" data-type="page" data-name="home">
+		<div id="background"></div>
+</section>
+
+</body>
+</html>
